5d6fd33718bc3cdb48af291d1bd2b2ee6f739d53
“YingZheng” <ikingdom778@gmail.com>
2023-04-03 15:18:21 +0900

    Initial Commit

---
+++ SwiftUI-CleanArchitecture/SwiftUI_CleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CleanArchitectureApp.swift
+//  SwiftUI-CleanArchitecture
+//
+//  Created by Hisashi Ishihara on 2023/04/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
